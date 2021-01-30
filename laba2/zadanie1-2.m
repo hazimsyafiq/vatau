cce8a09892406d18e4b125cf6c100911ece124d0
H. Syafiq <46275483+hazimsyafiq@users.noreply.github.com>
2021-01-30 22:58:21 +0800

    Upload zadanie1-2.m

---
+++ laba2/zadanie1-2.m
@@ -0,0 +1,25 @@
+figure(1) 
+plot(x,y1) 
+title('Function y=10+5^(-x)*cos(w*x+0.5)') 
+legend('w=1') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
+  
+
+figure(2) 
+plot(x,y2) 
+title('Function y=10+5^(-x)*cos(w*x+0.5)') 
+legend('w=2') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
+
+
+figure(3) 
+plot(x,y3) 
+title('Function y=10+5^(-x)*cos(w*x+0.5)') 
+legend('w=3') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
