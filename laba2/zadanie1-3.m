528226f9d82379fd67cc7fb25303c2fc4677bb13
H. Syafiq <46275483+hazimsyafiq@users.noreply.github.com>
2021-01-31 21:59:31 +0800

    Upload zadanie1-3

---
+++ laba2/zadanie1-3.m
@@ -0,0 +1,20 @@
+subplot(3,1,1), plot(x,y1) 
+title('function y when w=1') 
+legend('w=1') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
+
+subplot(3,1,2), plot(x,y2) 
+title('function y when w=2') 
+legend('w=2') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
+
+subplot(3,1,3), plot(x,y3) 
+title('function y when w=2') 
+legend('w=3') 
+xlabel('x') 
+ylabel('y(x)') 
+grid on 
